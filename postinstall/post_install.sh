a3da2159d525c29c96df88298d80004e236c666a
Alex Petrean <alex.petrean@outlook.com>
2021-05-30 03:56:43 +0300

    added stuff

--- postinstall/post_install.sh
+++ postinstall/post_install.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
 # This script handles the post install setup of my particular system on Endeavour OS
-
+ANTIGEN_DIR="$HOME/.antigen.d/"
 CONFIGS_DIR="$HOME/Configs"
 EMACS_DIR="$HOME/.emacs.d"
 DOOM="$EMACS_DIR/bin/doom"
 
+sudo pacman -S rustup
+
+rustup toolchain install stable
+
 yay -S paru pamac
 paru -R yay
 
-paru -S - < packages.txt
+paru -S - <packages.txt
+
+#change user shell to zsh
+chsh -s $(which zsh)
 
 # fetch antigen.zsh
-mkdir "$HOME/GitStuff"
-curl -L git.io/antigen > antigen.zsh
+mkdir "$ANTIGEN_DIR"
+curl -L git.io/antigen >"$ANTIGEN_DIR"/antigen.zsh
 
-# fetch my configs
-git clone https://github.com/alexpetrean80/configs.git $CONFIG_DIR
-cd $CONFIG_DIR
-
+#
 # fetch and install doom emacs
 git clone --depth 1 https://github.com/hlissner/doom-emacs $EMACS_DIR
 $DOOM install
+
+# fetch my configs
+git clone https://github.com/alexpetrean80/configs.git "$CONFIGS_DIR"
+cd $CONFIGS_DIR || exit
+stow ./*
+cd "$HOME" || exit
 
 # symlink all the configs to their usual places
 stow *
@@ -29,4 +39,5 @@ stow *
 # sync the emacs configs
 $DOOM sync
 
+# finish by rebooting
 sudo reboot now
